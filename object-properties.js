5ebad5f132e95c38c1aec65ba4e88eaa782d1097
anilkpatel <anil.patel@mail.mcgill.ca>
2020-11-18 16:20:21 +0000

    additional Nodeschool files for Mod 4 objects and functions

---
+++ object-properties.js
@@ -0,0 +1,42 @@
+const food = {
+  types: 'only pizza'
+}
+
+console.log(food.types);
+
+/*## OBJECT PROPERTIES (Exercise 16 of 20)  
+   
+  You can access and manipulate object properties –– the keys and values  
+  that an object contains –– using a method very similar to arrays.  
+   
+  Here's an example using square brackets:  
+   
+     const example = {  
+       pizza: 'yummy'  
+     }  
+       
+     console.log(example['pizza'])  
+   
+  The above code will print the string 'yummy' to the terminal.  
+   
+  Alternately, you can use dot notation to get identical results:  
+   
+     example.pizza  
+       
+     example['pizza']  
+   
+  The two lines of code above will both return yummy.  
+   
+ ## The challenge:  
+   
+  Create a file named object-properties.js.  
+   
+  In that file, define a variable named food like this:  
+   
+     const food = {  
+       types: 'only pizza'  
+     }  
+   
+  Use console.log() to print the types property of the food object to the  
+  terminal.  
+*/   
---
+++ objects.js
@@ -0,0 +1,15 @@
+const pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+}
+
+console.log(pizza);
+//define a variable named pizza
+//use console.log() to print pizza object to terminal
+
+//SOLUTION
+//Pizza object is a go
+
+//Returns:
+//Object { toppings: (3) […], crust: "deep dish", serves: 2 }
